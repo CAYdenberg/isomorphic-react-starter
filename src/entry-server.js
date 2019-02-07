8a0455b7d86c2dcc9484a3ec16f3b9e6bc971f61
@CAYdenberg <ydenberg@gmail.com>
2019-02-06 21:14:00 -0700

    Protect against XSS

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/server'
-import App from './components'
+import React from "react";
+import ReactDOM from "react-dom/server";
+import App from "./components";
+import xssFilters from "xss-filters";
 
-export default (req) => {
+export default req => {
   // determine the state (this may be async and require DB calls etc..)
   const state = {
-    email: req.query.email || '',
+    email: xssFilters.inHTMLData(req.query.email) || "",
     showValidation: !!req.query.email
-  }
+  };
 
   // render React to HTML string, passing the current state as a prop
-  const html = ReactDOM.renderToString(<App state={state} />)
+  const html = ReactDOM.renderToString(<App state={state} />);
 
   // Express action expects a Promise (so we can do async stuff in here)
-  return Promise.resolve({html, state})
-}
+  return Promise.resolve({ html, state });
+};
