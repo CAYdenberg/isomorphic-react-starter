0f62520041dea4a0bfb2e5093f6efcb8bd77dd80
@CAYdenberg <ydenberg@gmail.com>
2019-02-07 16:04:34 -0700

    Fix formatting

--- src/entry-server.js
+++ src/entry-server.js
@@ -4,10 +4,12 @@ import App from "./components";
 import xssFilters from "xss-filters";
 
 export default req => {
+  const email = req.query.email;
+
   // determine the state (this may be async and require DB calls etc..)
   const state = {
-    email: xssFilters.inHTMLData(req.query.email) || "",
-    showValidation: !!req.query.email
+    email: email ? xssFilters.inHTMLData(email) : "",
+    showValidation: !!email
   };
 
   // render React to HTML string, passing the current state as a prop
